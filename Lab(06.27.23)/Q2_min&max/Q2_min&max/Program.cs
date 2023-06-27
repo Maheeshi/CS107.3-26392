8b6bb62738c56e92abae2aa5faf75ffddf7a17ea
Maheeshi <132429336+Maheeshi@users.noreply.github.com>
2023-06-27 11:18:06 +0530

    Add files via upload

---
+++ Lab(06.27.23)/Q2_min&max/Q2_min&max/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_min_max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 122, 45, 65, 56, 49, 326, 15, 45, 945, 153 };
+
+            int smallest = num[0];
+            int biggest = num[0];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (smallest > num[i])
+                {
+                    smallest = num[i];
+                }
+                if (biggest < num[i])
+                {
+                    biggest = num[i];
+                }
+            }
+            Console.WriteLine("Smallest: " + smallest);
+            Console.WriteLine("Biggest: "+ biggest);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab(06.27.23)/Q3_ascending&descending/Q3_ascending&descending/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_ascending_descending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 122, 45, 56, 49, 326, 15, 45, 945, 153 };
+
+            for (int i = 0; i < num.Length; i++) 
+            {
+                for (int j = 0; j < num.Length - 1; j++)
+                {
+                    if (num[j] > num[i] + 1) 
+                    { 
+                        int temp = num[i];
+                        num[j] = num[j+1];  
+                        num[j + 1] = temp;
+                    }
+                        
+                }
+            }
+            for(int i = 0;i < num.Length; i++) 
+            {
+                Console.Write(num[i] + "");
+            }
+        }
+    }
+}
