385e996faa40afb475780bee8f1ccc5041f368f3
Maheeshi <132429336+Maheeshi@users.noreply.github.com>
2023-07-04 10:58:41 +0530

    Add files via upload

---
+++ Lab (4.7.23)/Q6-array/Q6-array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            array objarray = new array();
+            int[] myArray = objarray.arrayMethod(size); 
+            //through the object calling the array method
+            //return type is int[] 
+
+            Console.WriteLine("\nDisplay the array: ");
+            for (int i = 0; i < myArray.Length; i++) 
+            {
+                Console.Write(myArray[i] + "");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab (4.7.23)/Q6-array/Q6-array/array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6_array
+{
+    internal class array
+    {
+        public int[] arrayMethod(int size)
+        {
+            int[] myArray = new int[size];
+
+            for (int i = 0; i< myArray.Length; i += 2)
+            {
+                Console.WriteLine("Enter a valeu: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                myArray[i] = value;
+                myArray[i + 1] = 0;
+            }
+            return myArray;
+        }
+       
+    }
+}
